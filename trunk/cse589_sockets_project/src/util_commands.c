2aa49e4bd5e021f58768f57e965a801d07c35093
gassa0617@gmail.com <gassa0617@gmail.com@3cc37028-4f5e-f20a-44b0-110a0343d8e9>
2011-11-17 02:23:28 +0000

    test

--- trunk/cse589_sockets_project/src/util_commands.c
+++ trunk/cse589_sockets_project/src/util_commands.c
@@ -153,7 +153,8 @@ void ready() {
 					char ip[MAXLINE] = {'\0'};
 					char port[MAXLINE] = {'\0'};
 					get_connection_info(i, ip, port);
-					printf("\t Enter the initial token for %s", ip[MAXLINE]);
+					puts("==============>"+ip);
+					printf("\t Enter the initial token for %s \n", ip[MAXLINE]);
 					if (fgets(buffer, BUF_SIZE, stdin) == NULL) {
 						putchar('\n');
 						exit(1); /* Ctrl^D */
